fee3d216077ab9771fb65a0cfaa3ffb0924ac34d
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2025-12-16 22:16:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+import {ScrollTrigger, SplitText} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+const App = () => {
+    return (
+        <main></main>
+    );
+};
+export default App;
